97251dc81125a7ac4d2c24b6287b3cc98a2753a5
Thomas Junk <trj@fnal.gov>
2016-01-25 12:25:43 -0600

    Clean up file stubs and unclosed RootOutput files

--- dhtools/daq6deleter.sh
+++ dhtools/daq6deleter.sh
@@ -2,13 +2,20 @@
 
 scriptdir=/home/lbnedaq/trj
 remotenode=lbnedaq6
+
+# clean up transferred rootfiles from this directory
+
 dirtoclean=/storage/data/transferred
+
+# clean up unclosed files and stubs from this directory
+
+dirtoclean2=/storage/data
 
 source $scriptdir/senv.sh
 
 cd $localdonedir
 
-for filename in `ssh lbnedaq@${remotenode} find ${dirtoclean} -mtime +3 -name *.root`
+for filename in `ssh lbnedaq@${remotenode} find ${dirtoclean} -mtime +3 -name lbne*.root`
 do
   fbase=`basename $filename`
   samlocation=`samweb -e lbne locate-file $fbase`
@@ -25,4 +32,15 @@ do
     echo "Did not find location in enstore: " $samlocation
     echo "Not deleting file: " $filename
   fi
+done
+
+# clean up empty stub files -- no run or subrun number
+
+ssh lbnedaq@${remotenode} rm -f ${dirtoclean2}/lbne_r-_sr-_*.root
+
+# clean up files that were not closed properly
+
+for filename in `ssh lbnedaq@${remotenode} find ${dirtoclean2} -mtime +3 -name RootOutput*.root`
+do
+  rm -f $filename
 done
