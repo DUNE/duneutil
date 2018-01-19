52e4571a50b9ee9c75a410ddeb243da5858bcb5a
Tingjun Yang <tjyang@fnal.gov>
2018-01-18 19:03:56 -0600

    Change extractor_dict.py to be compatible with the version in larbatch (for validate_in_job).

--- python/extractor_dict.py
+++ python/extractor_dict.py
@@ -1,45 +1,124 @@
 #!/usr/bin/env python
 import sys, getopt
 import os
-import subprocess
 from subprocess import Popen, PIPE
-import time
-import samweb_cli
-#from samweb_client.utility import fileEnstoreChecksum
-import ast
+import threading
+import Queue
 import project_utilities, root_metadata
 import json
+import abc
 
-def getmetadata(inputfile, md0={}):
-        # Set up the experiment name for samweb Python API
-	samweb = samweb_cli.SAMWebClient(experiment=project_utilities.get_experiment())
+# Function to wait for a subprocess to finish and fetch return code,
+# standard output, and standard error.
+# Call this function like this:
+#
+# q = Queue.Queue()
+# jobinfo = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# wait_for_subprocess(jobinfo, q)
+# rc = q.get()      # Return code.
+# jobout = q.get()  # Standard output
+# joberr = q.get()  # Standard error
 
-	# Extract metadata into a pipe.
-	local = project_utilities.path_to_local(inputfile)
-	if local != '':
-		proc = subprocess.Popen(["sam_metadata_dumper", local], stdout=subprocess.PIPE)
-	else:
-		url = project_utilities.path_to_url(inputfile)
-		proc = subprocess.Popen(["sam_metadata_dumper", url], stdout=subprocess.PIPE)
-	if local != '' and local != inputfile:
-		os.remove(local)
+"""extractor_dict.py
+Purpose: To extract metadata from output file on worker node, generate JSON file
+"""
 
-	mdtext=''
-	for line in proc.stdout.readlines():
-		if line[-3:-1] != ' ,':
-			mdtext = mdtext + line.replace(", ,",",")
+
+class MetaData(object):
+    """Base class to hold / interpret general metadata"""
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __init__(self, inputfile):
+        self.inputfile = inputfile
+
+    def extract_metadata_to_pipe(self):
+        """Extract metadata from inputfile into a pipe for further processing."""
+        local = project_utilities.path_to_local(self.inputfile)
+        if len(local) > 0:
+            proc = Popen(["sam_metadata_dumper", local], stdout=PIPE,
+                         stderr=PIPE)
+        else:
+            url = project_utilities.path_to_url(inputfile)
+            proc = Popen(["sam_metadata_dumper", url], stdout=PIPE,
+                         stderr=PIPE)
+        if len(local) > 0 and local != self.inputfile:
+            os.remove(local)
+        return proc
+    
+    def get_job(self, proc):
+        """Run the proc in a 60-sec timeout queue, return stdout, stderr"""
+        q = Queue.Queue()
+        thread = threading.Thread(target=self.wait_for_subprocess, args=[proc, q])
+        thread.start()
+        thread.join(timeout=60)
+        if thread.is_alive():
+            print 'Terminating subprocess because of timeout.'
+            proc.terminate()
+            thread.join()
+        rc = q.get()
+        jobout = q.get()
+        joberr = q.get()
+        if rc != 0:
+            raise RuntimeError, 'sam_metadata_dumper returned nonzero exit status {}.'.format(rc)
+        return jobout, joberr
+    
+    @staticmethod
+    def wait_for_subprocess(jobinfo, q):
+        """Run jobinfo, put the return code, stdout, and stderr into a queue"""
+        jobout, joberr = jobinfo.communicate()
+        rc = jobinfo.poll()
+        for item in (rc, jobout, joberr):
+            q.put(item)
+        return
+
+    @staticmethod
+    def mdart_gen(jobtuple):
+        """Take Jobout and Joberr (in jobtuple) and return mdart object from that"""
+        mdtext = ''.join(line.replace(", ,", ",") for line in jobtuple[0].split('\n') if line[-3:-1] != ' ,')
 	mdtop = json.JSONDecoder().decode(mdtext)
-	if len(mdtop.keys()) == 0:
-		print 'No top-level key in extracted metadata.'
-		sys.exit(1)
-	file_name = mdtop.keys()[0]
-	mdart = mdtop[file_name]
+        if len(mdtop.keys()) == 0:
+            print 'No top-level key in extracted metadata.'
+            sys.exit(1)
+        file_name = mdtop.keys()[0]
+        return mdtop[file_name]
 
-	# define an empty python dictionary which will hold sam metadata.
-	# Some fields can be copied directly from art metadata to sam metadata.
-	# Other fields require conversion.
+    @staticmethod
+    def md_handle_application(md):
+        """If there's no application key in md dict, create the key with a blank dictionary.
+        Then return md['application'], along with mdval"""
+        if 'application' not in md:
+            md['application'] = {}
+        return md['application']
+
+
+class expMetaData(MetaData):
+    """Class to hold/interpret experiment-specific metadata"""
+    def __init__(self, expname, inputfile):
+        MetaData.__init__(self, inputfile)
+        self.expname = expname
+        #self.exp_md_keyfile = expname + '_metadata_key'
+        try:
+            #translateMetaData = __import__("experiment_utilities", "MetaDataKey")
+	    from experiment_utilities import MetaDataKey
+        except ImportError:
+            print "You have not defined an experiment-specific metadata and key-translating module in experiment_utilities. Exiting"
+            raise
+	    
+        metaDataModule = MetaDataKey()
+	self.metadataList, self.translateKeyf = metaDataModule.metadataList(), metaDataModule.translateKey
+
+    def translateKey(self, key):
+        """Returns the output of the imported translateKey function (as translateKeyf) called on key"""
+        return self.translateKeyf(key)
+
+    def md_gen(self, mdart, md0={}):
+        """Loop through art metdata, generate metadata dictionary"""
+        # define an empty python dictionary which will hold sam metadata.
+        # Some fields can be copied directly from art metadata to sam metadata.
+        # Other fields require conversion.
 	md = {}
-
+	
 	# Loop over art metadata.
 	for mdkey in mdart.keys():
 		mdval = mdart[mdkey]
@@ -134,15 +213,44 @@ def getmetadata(inputfile, md0={}):
 		else:
 			md[mdkey] = mdart[mdkey]
 
-	# Get the other meta data field parameters						
-	md['file_name'] =  inputfile.split("/")[-1]
-	md['file_size'] =  os.path.getsize(inputfile)
-	md['crc'] = root_metadata.fileEnstoreChecksum(inputfile)
-	return md
+	# Get the other meta data field parameters				
+
+        md['file_name'] = self.inputfile.split("/")[-1]
+        if 'file_size' in md0:
+            md['file_size'] = md0['file_size']
+        else:
+            md['file_size'] = os.path.getsize(self.inputfile)
+        if 'crc' in md0:
+            md['crc'] = md0['crc']
+        else:
+            md['crc'] = root_metadata.fileEnstoreChecksum(self.inputfile)
+
+        # In case we ever want to check out what md is for any instance of MetaData by calling instance.md
+        self.md = md
+        return self.md
+
+    def getmetadata(self, md0={}):
+        """ Get metadata from input file and return as python dictionary.
+        Calls other methods in class and returns metadata dictionary"""
+        proc = self.extract_metadata_to_pipe()
+        jobt = self.get_job(proc)
+        mdart = self.mdart_gen(jobt)
+        return self.md_gen(mdart, md0)	
+
+def main():
+    try:
+        expSpecificMetadata = expMetaData(os.environ['SAM_EXPERIMENT'], str(sys.argv[1]))
+    except TypeError:
+        print 'You have not implemented a defineMetaData function by providing an experiment.'
+        print 'No metadata keys will be saved'
+        raise
+    mdtext = json.dumps(expSpecificMetadata.getmetadata(), indent=2, sort_keys=True)
+    print mdtext
+    sys.exit(0)
+
+
 
 if __name__ == "__main__":
-	md = getmetadata(str(sys.argv[1]))
-	#print md	
-	mdtext = json.dumps(md, indent=2, sort_keys=True)
-	print mdtext
-	sys.exit(0)	
+    main()
+
+
