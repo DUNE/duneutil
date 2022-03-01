e3bdcac856140aa536ef1c5711aa0a4e179adfd6
Jacob Calcutt <calcuttj@msu.edu>
2022-03-01 13:25:43 -0600

    new_cache_state: Adding ability to check file list. Added py script to list dependencies in dunesw suite

---
+++ scripts/list_dependencies.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+import os
+
+suite = ['dunecore', 'duneana', 'dunesw', 'dunecalib', 'dunesim', 'duneprototypes',
+         'dunereco', 'protoduneana', 'duneopdet', 'dunedataprep',
+         'duneexamples', 'duneutil']
+dep_files = ['%s/%s/ups/product_deps'%(os.environ['MRB_SOURCE'], s) for s in suite]
+print(dep_files)
+
+
+deps = dict() 
+for df in dep_files:
+  with open(df, 'r') as f:
+    lines = f.readlines()
+    found_header = False
+    for l in lines:
+      if len(l.split()) > 1 and l.split()[0] == 'product' and l.split()[1] == 'version':
+        found_header = True
+        continue
+      elif 'end_product_list' in l:
+        break
+      if found_header:
+        if l.split()[0] in deps.keys(): continue
+        deps[l.split()[0]] = l.split()[1]
+print()
+for k, v in deps.items():
+  print(k, v)
