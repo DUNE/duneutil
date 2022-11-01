90d1bdc1856fd7e2f007b84c90d7b5f8d839d8c6
Jacob Calcutt <calcuttj@msu.edu>
2022-11-01 11:41:29 -0500

    Adding multiprocessing to new_cache_state

--- python/new_cache_state.py
+++ python/new_cache_state.py
@@ -7,7 +7,48 @@ import samweb_client as swc
 import pycurl
 import subprocess
 import shlex
+import multiprocessing as mp
 
+def check_files(thislist, cached, pending, a):
+  sam = swc.SAMWebClient("dune")
+  i = 0
+  for f in thislist:
+    if not (i%100): print("Locating files: %i/%i"%(i, len(thislist)))
+    check_file(f, sam, cached, pending, a)
+    i += 1
+    #locs = sam.locateFile(f)
+    #for l in locs:
+    #  split_path = l['full_path'].split(':')
+    #  if split_path[0] == 'enstore':
+    #    if args.method == "pnfs":
+    #      this_cached = cache_state.is_file_online_pnfs(os.path.join(split_path[1], f))
+    #      if args.verbose:
+    #        print( f, "ONLINE" if this_cached else "NEARLINE")
+    #      if this_cached: cached += 1
+    #      break 
+    #    else:
+    #      qos,targetQos = cache_state.get_file_qos(c, os.path.join(split_path[1], f))
+    #      if "ONLINE" in qos: cached += 1 
+    #      if "disk" in targetQos: pending += 1
+    #a += 1
+
+def check_file(f, sam, cached, pending, a):
+  locs = sam.locateFile(f)
+  for l in locs:
+    split_path = l['full_path'].split(':')
+    if split_path[0] == 'enstore':
+      if args.method == "pnfs":
+        this_cached = cache_state.is_file_online_pnfs(
+            os.path.join(split_path[1], f))
+        if args.verbose:
+          print( f, "ONLINE" if this_cached else "NEARLINE")
+        if this_cached: cached.value += 1
+        break 
+      else:
+        qos,targetQos = cache_state.get_file_qos(c, os.path.join(split_path[1], f))
+        if "ONLINE" in qos: cached.value += 1 
+        if "disk" in targetQos: pending.value += 1
+  a.value += 1
 
 examples = ''
 parser= argparse.ArgumentParser(epilog=examples, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -35,6 +76,7 @@ parser.add_argument("-ss", "--snapshot", dest="snapshot", help="[Also requires -
 parser.add_argument("-v","--verbose", action="store_true", dest="verbose", default=False, help="Print information about individual files")
 parser.add_argument("-p","--prestage", action="store_true", dest="prestage", default=False, help="Prestage the files specified")
 parser.add_argument("-m", "--method", choices=["rest", "pnfs"], default="rest", help="Use this method to look up file status.")
+parser.add_argument('-n', type=int, default=1, help='n procs')
 
 args=parser.parse_args()
 
@@ -66,32 +108,48 @@ try:
   elif len(args.files) > 0:
     thislist = args.files
   print(len(thislist))
+
+
   samlist = []
-  a = 0
-  cached = 0
-  pending = 0
+  #a = 0
+  #cached = 0
+  #pending = 0
+  a = mp.Value('i', 0)
+  cached = mp.Value('i', 0)
+  pending = mp.Value('i', 0)
+
   c = cache_state.make_curl() if args.method == "rest" else None
-  for f in thislist:
-    if not (a%100): print("Locating files: %i/%i"%(a, len(thislist)), end='\r')
-    locs = sam.locateFile(f)
-    for l in locs:
-      split_path = l['full_path'].split(':')
-      if split_path[0] == 'enstore':
-        if args.method == "pnfs":
-          this_cached = cache_state.is_file_online_pnfs(os.path.join(split_path[1], f))
-          if args.verbose:
-            print( f, "ONLINE" if this_cached else "NEARLINE")
-          if this_cached: cached += 1
-          break 
-        else:
-          qos,targetQos = cache_state.get_file_qos(c, os.path.join(split_path[1], f))
-          if "ONLINE" in qos: cached += 1 
-          if "disk" in targetQos: pending += 1
-    a += 1
+
+  split_list = [thislist[i::args.n] for i in range(args.n)]
+  procs = [
+      mp.Process(target=check_files, args=(split_list[i], cached, pending, a))
+      for i in range(args.n)
+  ]
+
+  for p in procs: p.start()
+  for p in procs: p.join()
+
+  #for f in thislist:
+  #  if not (a%100): print("Locating files: %i/%i"%(a, len(thislist)), end='\r')
+  #  locs = sam.locateFile(f)
+  #  for l in locs:
+  #    split_path = l['full_path'].split(':')
+  #    if split_path[0] == 'enstore':
+  #      if args.method == "pnfs":
+  #        this_cached = cache_state.is_file_online_pnfs(os.path.join(split_path[1], f))
+  #        if args.verbose:
+  #          print( f, "ONLINE" if this_cached else "NEARLINE")
+  #        if this_cached: cached += 1
+  #        break 
+  #      else:
+  #        qos,targetQos = cache_state.get_file_qos(c, os.path.join(split_path[1], f))
+  #        if "ONLINE" in qos: cached += 1 
+  #        if "disk" in targetQos: pending += 1
+  #  a += 1
   print()
   print(len(samlist))
 
-  print("%i/%i files are cached"%(cached, a))
+  print("%i/%i files are cached"%(cached.value, a.value))
 
   #filelist = enstore_locations_to_paths(list(samlist), args.sparse) 
   print( " done." )
